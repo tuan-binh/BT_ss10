f3baaf95d1bbc4a2334eede22426348da90a13f4
tuan-binh <binh1892001@gmail.com>
2023-06-20 17:05:25 +0700

    upload

--- src/bt14/Main.java
+++ src/bt14/Main.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+
+        int size = (int) Math.ceil(Math.random() * 10);
+
         ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < size; i++) {
             ArrayList<Integer> row = new ArrayList<>();
-            for (int j = 0; j < 4; j++) {
+            for (int j = 0; j < size; j++) {
                 row.add((int) Math.ceil(Math.random() * 9));
             }
             matrix.add(row);
